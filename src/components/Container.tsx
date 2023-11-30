491bf6e52fcf113ea3ab36a4eb313cc03082611b
Yugma Dewangga <yugmade@gmail.com>
2023-11-30 17:30:42 +0700

    change container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 function Container({ children }: { children: ReactNode }) {
   return (
-    <main className="relative bg-white w-full min-h-screen">
+    <main className="relative bg-white w-full min-h-screen overflow-x-hidden">
       {children}
     </main>
   );
